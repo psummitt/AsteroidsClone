0e6d087fa83aaa70cae32d7c4a9703d1ff565f1d
psummitt <psummitt@gmail.com>
2022-12-07 16:54:19 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
+const FPS = 30; // frames per second
+const SHIP_SIZE = 30; // ship height in pixels
+
+
 /** @type {HTMLCanvasElement'} */
 var canv = document.getElementById("gameCanvas");
 var ctx = canv.getContext("2d");
 
+var ship = {
+    x: canv.width / 2,
+    y: canv.height / 2,
+    r: SHIP_SIZE / 2,
+    a: 90 /180 * Math.PI // convert to radians
+}
+
+// set up the game loop
+setInterval(update, 1000/FPS);
+
+function update() {
+    // draw space
+    
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canv.width, canv.height);
+
+    // draw ship
+    ctx.strokeStyle
+
+
+    // rotate ship
+
+    // move ship 
+}
